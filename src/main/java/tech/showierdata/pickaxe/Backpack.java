2a83a393ecee73d753899892fd00ea7169c18f79
ShowierData9978 <mellfang36@gmail.com>
2023-08-01 23:05:54 -0500

    Update version (and add files i forgot to add)

---
+++ src/main/java/tech/showierdata/pickaxe/Backpack.java
@@ -0,0 +1,25 @@
+package tech.showierdata.pickaxe;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SkullItem;
+
+@SuppressWarnings("unused")
+public class Backpack {
+	int id;
+	int i;
+
+	Inventory inventory;
+	ItemStack head;
+
+	public Backpack(int id, int i, Inventory inventory, ItemStack head) {
+		this.id = id;
+		this.i = i;
+		this.inventory = inventory;
+		this.head = head;
+
+		assert head.getItem() instanceof SkullItem;
+
+
+	}
+}
