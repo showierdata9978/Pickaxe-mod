afc70abbba30e01c52e42a9fa24029e53b8e370c
MystWraith <77246251+MystWraith@users.noreply.github.com>
2023-08-24 15:37:44 -0700

    Fixed odd hunger bar glitch (#25)
    * Fixed odd hunger bar glitch
    
    Also made the code a lot simpler and compact.
    
    * Removed useless comment
    
    * Removed useless log param
    
    * Change to 10
    
    * Variety of updates
    
    - Added a removal of the mount health bar.
    - Added a removal of the breath bar
    
    * No prints, sorry
    
    * Alt method comment
    
    The comment that was added was an alternate way to do roughly the same thing, but there are interesting consequences.
    
    * Wrong one commented, mb
    
    ---------
    
    Co-authored-by: ShowierData9978 <mellfang36@gmail.com>

--- src/main/java/tech/showierdata/pickaxe/mixin/InGameHudMixin.java
+++ src/main/java/tech/showierdata/pickaxe/mixin/InGameHudMixin.java
@@ -7,8 +7,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import tech.showierdata.pickaxe.Pickaxe;
 
@@ -16,48 +19,30 @@ import tech.showierdata.pickaxe.Pickaxe;
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
 
+    /*@ModifyVariable(method = "renderStatusBars", at = @At(value = "STORE", ordinal = 0))
+    PlayerEntity modifPlayerEntity(PlayerEntity playerEntity) {
+        if (Pickaxe.getInstance().isInPickaxe()) return null;
+        return playerEntity;
+    }*/
 
-    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 3), index = 5)
-    private int modifyHungerQuad1(int value) {
-        if (Pickaxe.getInstance().isInPickaxe()) {
-            return 0;
-        }// set the width to 0
-        return value;
+    @ModifyConstant(method = "renderStatusBars", constant = @Constant(intValue = 0, ordinal = 2))
+    private int modifyHungerLoop(int zero) {
+        if (Pickaxe.getInstance().isInPickaxe()) return 10;
+        return zero;
     }
 
-    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 3), index = 6)
-    private int modifyHungerQuad2(int value) {
-        if (Pickaxe.getInstance().isInPickaxe()) {
-            return 0;
-        }// set the width to 0
-        return value;
+    @ModifyVariable(method = "renderStatusBars", slice = @Slice(from = @At(value = "INVOKE", target = "net/minecraft/entity/player/PlayerEntity.getMaxAir ()I")), at = @At(value = "STORE", ordinal = 0), ordinal = 14)
+    private int modifyAirBar(int y) {
+        if (Pickaxe.getInstance().isInPickaxe()) return 0;
+        return y;
     }
 
-    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 1 + 3), index = 5)
-    private int modifyHungerQuad3(int value) {
-        if (Pickaxe.getInstance().isInPickaxe()) return 0; // set the width to 0
-        return value;
+    @ModifyVariable(method = "renderMountHealth", slice = @Slice(from = @At(value = "INVOKE", target = "net/minecraft/client/gui/hud/InGameHud.getHeartCount (Lnet/minecraft/entity/LivingEntity;)I")), at = @At(value = "STORE", ordinal = 0), ordinal = 0)
+    private int modifyMountBar(int o) {
+        if (Pickaxe.getInstance().isInPickaxe()) return 0;
+        return o;
     }
 
-        @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 1 + 3), index = 6)
-    private int modifyHungerQuad4(int value) {
-        if (Pickaxe.getInstance().isInPickaxe()) return 0; // set the width to 0
-        return value;
-    }
-
-
-
-    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 3 + 3), index = 5)
-    private int modifyHungerQuad5(int value) {
-        if (Pickaxe.getInstance().isInPickaxe()) return 0; // set the width to 0
-        return value;
-    }   
-    
-    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 3 + 3), index = 6)
-    private int modifyHungerQuad6(int value) {
-        if (Pickaxe.getInstance().isInPickaxe()) return 0; // set the width to 0
-        return value;
-    }
     @Inject(at=@At("TAIL"), method = "renderHotbarItem")
     private void renderHotbarIcons(DrawContext context, int x, int y, float f, PlayerEntity player, ItemStack stack, int seed, CallbackInfo ci) {
         Pickaxe.getInstance().renderHotbarIcons(context, x, y, stack);
