601b61bff44b85265932922cc59b21b3a9e7d889
MystWraith <77246251+MystWraith@users.noreply.github.com>
2023-08-23 21:41:41 -0700

    Added the mixin and am ready to test
    Further debug will occur

--- src/main/java/tech/showierdata/pickaxe/mixin/MessageHandlerMixin.java
+++ src/main/java/tech/showierdata/pickaxe/mixin/MessageHandlerMixin.java
@@ -14,21 +14,20 @@ import tech.showierdata.pickaxe.config.Options;
 import tech.showierdata.pickaxe.server.Plot;
 import tech.showierdata.pickaxe.server.Regexs;
 
-@SuppressWarnings("UnusedMixin")
 @Mixin(MessageHandler.class)
 public class MessageHandlerMixin {
 	@Inject(at = @At("HEAD"), method = "onChatMessage", cancellable = true)
     private void onChatMessage(@NotNull SignedMessage message, GameProfile sender, MessageType.Parameters params, CallbackInfo info) {
-		if (Regexs.isLocateCommand(message.getContent().getString())) {
+		/*if (Regexs.isLocateCommand(message.getContent().getString())) {
 			if (Pickaxe.commandHelper.getLastSentCommand().equals("locate")) {
 				Pickaxe.commandHelper.clearLastSentCommand();
 				Plot plot = Regexs.getPlotDetails(message.getContent().getString());
 				/*pickaxe.currentPlot = plot;*/
-				assert plot != null;
+				/*assert plot != null;
 				Pickaxe.LOGGER.info("Located plot: " + plot.name);
 				info.cancel(); // stop the message from being shown to the player.
 			}
-		}
+		}*/
 		if (Regexs.isPlotAd(message.getContent().getString())) {
 			if (!Options.getInstance().hide_plot_ads) return;
 			if (!Pickaxe.getInstance().isInPickaxe()) return;
